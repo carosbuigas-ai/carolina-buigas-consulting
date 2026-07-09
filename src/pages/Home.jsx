f3e6d5aba324614f61f0452d973637d17840502e
carosbuigas-ai <caro.s.buigas@gmail.com>
2026-07-09 14:37:32 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import BackgroundLayer from "@/components/site/BackgroundLayer";
-import HeroSection from "@/components/site/HeroSection";
-import ProofStrip from "@/components/site/ProofStrip";
-import GapSection from "@/components/site/GapSection";
-import WhatIDoSection from "@/components/site/WhatIDoSection";
-import EdgeSection from "@/components/site/EdgeSection";
-import FounderNoteSection from "@/components/site/FounderNoteSection";
-import BuiltWithSection from "@/components/site/BuiltWithSection";
-import WaysToBuildSection from "@/components/site/WaysToBuildSection";
-import FoundersSection from "@/components/site/FoundersSection";
-import FinalCtaSection from "@/components/site/FinalCtaSection";
+import BackgroundLayer from "../components/site/BackgroundLayer";
+import HeroSection from "../components/site/HeroSection";
+import ProofStrip from "../components/site/ProofStrip";
+import GapSection from "../components/site/GapSection";
+import WhatIDoSection from "../components/site/WhatIDoSection";
+import EdgeSection from "../components/site/EdgeSection";
+import FounderNoteSection from "../components/site/FounderNoteSection";
+import BuiltWithSection from "../components/site/BuiltWithSection";
+import WaysToBuildSection from "../components/site/WaysToBuildSection";
+import FoundersSection from "../components/site/FoundersSection";
+import FinalCtaSection from "../components/site/FinalCtaSection";
 
 export default function Home() {
   return (
